9b16fcbb4a3243e097ce60934a5d4c3b9e2fc447
AKSB-GP <alexander.kazakov1999@gmail.com>
2024-12-22 21:00:35 +0100

    redesigned version handling

--- app.py
+++ app.py
@@ -6,147 +6,136 @@ import tkinter.ttk as ttk
 import webbrowser
 from manager import BlenderManager
 from constants import *
+from pathlib import Path
+
 """
-Add a app icon and built it
+Add JSON handling for:
+adding and removing version from launcher
+change scan_version to not run automatically
 """
+
+#class Usersettings
 
 class BlenderLauncherApp:
     """Main App and its widgets"""
-
     def __init__(self, root):
         self.root = root
         self.root.title("Blender Launcher")
         self.root.geometry("400x400")
         self.root.configure(bg="darkgray")
-        self.manager = BlenderManager()  # Used to handle blender versions
-        
-        self.selected_version = tk.StringVar(self.root) # text for selected version, defaults to 'Select version'
+        #Class Used to handle blender versions
+        self.manager = BlenderManager()          
+        #text for selected version, defaults to 'Select version'when launcher first runs
+        self.selected_version = tk.StringVar(self.root)
         self.selected_version.set("Select version")
         self.starting_selection_text ="Select version"
         self.run_setup()
 
-    """METHODS TO LOAD widgets AND STYLES"""
     def run_setup(self):
-        #Scan for blender versions already installed
-        self.scan_for_Versions()
-
-        # render UI
+        #Render UI
         self.setup_styles()
         self.create_widgets()
         self.create_widgets()
+        #Update dropdown for Blender versions
+        self.update_dropdown()
         
     def setup_styles(self):
-        """ 'Stylesheet' for widgets """
+        """ Stylesheet for widgets"""
         style = ttk.Style()
         style.configure("TButton", font=("Yu Gothic Medium", 14), background='black', foreground="black")
         style.configure("TLabel", font=("Yu Gothic Medium", 14),background='gray',foreground="red")
         style.configure("TMenubutton", font=("Yu Gothic Medium", 14), background='black', foreground="white")
         
-    def scan_for_Versions(self):
-        """Scans if versions are already installed"""
-        if self.manager.find_existing_versions():
-            self.update_dropdown()
-        else:
-            pass
-        
-        
-
     def create_widgets(self):
         """Create and position all widgets"""
-        # the Dropdown for selecting  versions
+        #the Dropdown for selecting  versions
         self.dropdown = ttk.OptionMenu(self.root, self.selected_version, "Select version", *self.manager.get_all_versions())
         self.dropdown.pack(pady=10, padx=10)
         self.dropdown.place(y=40, x=40)
-        
-        # the Launch button
+        #the Launch button
         launch_button = ttk.Button(self.root, text="Start", command=self.launch_blender)
         launch_button.pack(pady=10, padx=10)
         launch_button.place(y=80, x=40)
-        # the Open folder button
+        #the Open folder button
         folder_button = ttk.Button(self.root, text="Open Folder", command=self.open_folder)
         folder_button.pack(pady=10, padx=10)
         folder_button.place(y=120, x=40)
-        
-        # the version button
+        #the version button
         add_button = ttk.Button(self.root, text="Add Version", command=self.add_version)
         add_button.pack(pady=10, padx=10)
         add_button.place(y=160, x=40)
-
-        # the Download button
+        #the Download button
         download_button = ttk.Button(self.root, text="Download a version", command=self.open_download_link)
         download_button.pack(pady=10, padx=10)
         download_button.place(y=200,x=40)
-        
-        # The remove version button:        
+        #The remove version button:        
         remove_button = ttk.Button(self.root, text="Remove version", command=self.remove_version)
         remove_button.pack(pady=10, padx=10)
         remove_button.place(y=240, x=40)
         
-
+    """-----------------------Methods used in interactions----------------------------"""
       
-    """METHOD TO HANDLE LOGIC AND INTERACTION"""
-    
-        
+                
     def open_download_link(self):
         """Opens the link to the blender.org download page"""
         webbrowser.open_new_tab(BLENDER_DOWNLOAD_URL)
 
     
     def add_version(self):
-        """Prompts user to open a folder for the version they want to add """
+        """Prompts user to open a folder for the version they want to add"""
         folder_select = filedialog.askdirectory(initialdir=BLENDER_FOUNDATION_PATH)
-        
+        #updates dropdown to include added version
         if self.manager.add_version(folder_select):
-            self.update_dropdown()  # updates dropdown to include added version
-        else:
-            messagebox.showerror("Invalid folder", "Invalid folder. Please ensure that the parent folder is 'Blender Foundation' ")
-            return
-    
+            self.update_dropdown()
+        if "blender" not in folder_select.lower() or "blender foundation" not in folder_select.lower():
+            messagebox.showerror("Invalid folder selected", "Folder doesnt contain blender or blender foundation in the filepath, aborting...'")
+        
     def remove_version(self):
         """Removes version from launcher, note! it does not remove from computer"""
         version = self.selected_version.get()
+        #if version is the starting selection text 
         if version ==self.starting_selection_text:
             messagebox.showwarning("No version selected",text="Please select a version to delete")
             return
-        confirmation = messagebox.askyesno("Remove Version", f"Are you sure you want to remove {version}?")
-
+        confirmation = messagebox.askyesno("Remove Blender Version", f"Are you sure you want to remove {version}?")
+        #if user confirms, remove version
         if confirmation:
-            if version in self.manager.versions:
-                self.manager.versions.pop(version)
+            if self.manager.remove_version(version):
                 self.selected_version.set(self.starting_selection_text)
                 self.update_dropdown()
             else:
                 messagebox.showinfo("Version not found",text="Version was not found, maybe it has already been deleted?")
-                
-    def save_version(self):
-        #call through manager
-          print("WP")
    
     def update_dropdown(self):
-        """Updates the dropdown menu to ensure all versions are shown """
+        """Updates the dropdown menu to ensure all versions are shown"""
         menu = self.dropdown["menu"]
         menu.delete(0, "end")
+        all_versions = self.manager.get_all_versions()
+        #Check if there are any versions
+        if not all_versions:
+            self.selected_version.set("No version in launcher")
+        if all_versions and self.selected_version.get() =="No version in launcher":
+            self.selected_version.set(self.starting_selection_text)    
         for version in self.manager.get_all_versions():
             menu.add_command(label=version, command=lambda v=version: self.selected_version.set(v))
 
     def open_folder(self):
         """Opens the folder of the selected Blender version"""
         version = self.selected_version.get()
-        
         if version == self.starting_selection_text:
             messagebox.showwarning("Warning", "Please select a version of Blender.")
             return
-        
-        path = self.manager.get_version_path(version)
+        path = Path(self.manager.get_version_path(version)).parent
         if path:
             folder = os.path.dirname(path)
-            subprocess.Popen(f'explorer "{folder}"')
+            subprocess.Popen(f'explorer "{path}"')
         else:
             messagebox.showerror("Invalid version selected", "You have selected an invalid Blender version.")
 
     def launch_blender(self):
         """Launches the selected Blender version"""
         version = self.selected_version.get()
+        #Check if version selection is correct and launch blender based on path
         if version in self.manager.get_all_versions():
             path = self.manager.get_version_path(version)
             if path:
@@ -163,8 +152,6 @@ class BlenderLauncherApp:
             messagebox.showwarning("Warning", "Please select a valid Blender version.")
             return
     
-
-
 if __name__ == "__main__":
     root = tk.Tk()
     app = BlenderLauncherApp(root)
--- manager.py
+++ manager.py
@@ -3,46 +3,59 @@ import tkinter as tk
 from tkinter import messagebox
 import tkinter.ttk as ttk
 from constants import *
-
+import json
 class BlenderManager:
-    """Class to handle versions and related"""
+    """Class to handle versions"""
+    SETTINGS_FILE = SETTINGS
     
     def __init__(self):
         """Stores Blender versions"""
         self.versions = {
         }
-        self.find_existing_versions()
+        self.load_settings()
+
+    def load_settings(self):
+        '''Load the settings json'''
+        if os.path.exists(self.SETTINGS_FILE):
+            with open(self.SETTINGS_FILE,"r") as file:
+                settings = json.load(file)
+                self.versions = settings.get("blender_versions",{})
+        else:
+             self.versions = {}   
+
+    def save_settings(self):
+        '''Save versions to settings json'''
+        with open(self.SETTINGS_FILE, "w") as file:
+            json.dump({"blender_versions":self.versions},file,indent=4)
+
 
     def add_version(self, folder_path):
-        """Adds a new Blender version to version dict"""
-        tail, version_name = os.path.split(folder_path)
-        if tail == BLENDER_FOUNDATION_PATH:
-            exe_path = os.path.join(folder_path, "blender.exe").replace("\\", "/")
-            self.versions[version_name] = os.path.normpath(exe_path)
+        """
+        Adds a new Blender version to version dict, 
+        by selecting the blender version folder
+        """
+        version_name = os.path.basename(folder_path)
+        blender_executable = os.path.join(folder_path, "blender.exe")
+
+        if os.path.exists(blender_executable):
+            self.versions[version_name] = blender_executable
+            self.save_settings()
             return True
-        else:
-            return False
+        return False
+
+    def remove_version(self,version_name):
+        '''Remove a version from json based on the version_name'''
+        if version_name in self.versions:
+            del self.versions[version_name]
+            self.save_settings()
+            return True
+        return False
     
-  
-
-    def get_version_path(self, version_name):
+    def get_version_path(self, version):
         """Returns the executable path for a version"""
-        return self.versions.get(version_name, None)
-
-    def find_existing_versions(self):
-        install_root_path = BLENDER_FOUNDATION_PATH
-        #Check if blender_foundation folder evem exists
-        if install_root_path:
-            for folder in os.listdir(install_root_path):
-                version_folder= os.path.join(install_root_path,folder).replace("\\", "/")
-                if os.path.isdir(version_folder) and "blender.exe" in os.listdir(version_folder):
-                    self.add_version(version_folder)
-        else:
-            messagebox.showinfo("No versions installed",text="You have no versions installed of Blender")
-            
+        return self.versions.get(version)
 
     def get_all_versions(self):
         """Returns all versions by keys, I.e versions"""
-
         return list(self.versions.keys())
     
